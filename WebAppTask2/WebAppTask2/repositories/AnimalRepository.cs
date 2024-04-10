6ef84e91a3d228ac2ce455fab7e20fc3b2dc2a9c
s25686 <s25686@pjwstk.edu.pl>
2024-04-10 22:43:14 +0200

    Implement Animal and Visit repositories with CRUD operations

--- WebAppTask2/WebAppTask2/repositories/AnimalRepository.cs
+++ WebAppTask2/WebAppTask2/repositories/AnimalRepository.cs
@@ -1,6 +1,41 @@
+using WebAppTask2.Interfaces;
+using WebAppTask2.Models;
+
 namespace WebAppTask2.repositories;
 
-public class AnimalRepository
+public class AnimalRepository : IAnimalRepository
 {
-    
+    private List<Animal> _animals = new List<Animal>();
+
+    public void Add(Animal animal)
+    {
+        _animals.Add(animal);
+    }
+
+    public bool Delete(Animal animal)
+    {
+        return _animals.Remove(animal);
+    }
+
+    public void Edit(Animal animal)
+    {
+        var existingAnimal = _animals.FirstOrDefault(a => a.Id == animal.Id);
+        if (existingAnimal != null)
+        {
+            existingAnimal.Name = animal.Name;
+            existingAnimal.Category = animal.Category;
+            existingAnimal.Weight = animal.Weight;
+            existingAnimal.FurColor = animal.FurColor;
+        }
+    }
+
+    public Animal Get(int id)
+    {
+        return _animals.FirstOrDefault(a => a.Id == id);
+    }
+
+    public Task<IEnumerable<Animal>> GetAll()
+    {
+        return Task.FromResult(_animals.AsEnumerable());
+    }
 }
--- WebAppTask2/WebAppTask2/repositories/VisitRepository.cs
+++ WebAppTask2/WebAppTask2/repositories/VisitRepository.cs
@@ -1,6 +1,50 @@
-namespace WebAppTask2.repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppTask2.Interfaces;
+using WebAppTask2.Models;
 
-public class VisitRepository
+namespace WebAppTask2.repositories
 {
-    
+    public class VisitRepository : IVisitRepository
+    {
+        private List<Visit> _visits = new List<Visit>();
+
+        public void Add(Visit visit)
+        {
+            _visits.Add(visit);
+        }
+
+        public void Update(Visit visit)
+        {
+            var existingVisit = _visits.FirstOrDefault(v => v.Id == visit.Id);
+            if (existingVisit != null)
+            {
+                existingVisit.DateOfVisit = visit.DateOfVisit;
+                existingVisit.Price = visit.Price;
+                // Update other properties as needed
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var visitToRemove = _visits.FirstOrDefault(v => v.Id == id);
+            if (visitToRemove != null)
+            {
+                _visits.Remove(visitToRemove);
+            }
+        }
+
+        public Task<IEnumerable<Visit>> GetAllForAnimal(int animalId)
+        {
+            var animalVisits = _visits.Where(v => v.AnimalId == animalId).ToList();
+            return Task.FromResult(animalVisits.AsEnumerable());
+        }
+
+        public Task<Visit?> GetById(int id)
+        {
+            var visit = _visits.FirstOrDefault(v => v.Id == id);
+            return Task.FromResult<Visit?>(visit);
+        }
+    }
 }
