92b1ba0e2116c8f90c5d98c9e7c76c587dddbd43
s25686 <s25686@pjwstk.edu.pl>
2024-04-10 22:44:33 +0200

    IAnimalRepository and IVisitRepository interfaces for CRUD operations

--- WebAppTask2/WebAppTask2/Interfaces/IAnimalRepository.cs
+++ WebAppTask2/WebAppTask2/Interfaces/IAnimalRepository.cs
@@ -1,6 +1,12 @@
+using WebAppTask2.Models;
+
 namespace WebAppTask2.Interfaces;
 
 public interface IAnimalRepository
 {
-    
+    Task<IEnumerable<Animal>> GetAll();
+    Animal Get(int id);
+    void Add(Animal animal);
+    void Edit(Animal animal);
+    bool Delete(Animal animal);
 }
--- WebAppTask2/WebAppTask2/Interfaces/IVisitRepository.cs
+++ WebAppTask2/WebAppTask2/Interfaces/IVisitRepository.cs
@@ -1,6 +1,14 @@
-namespace WebAppTask2.Interfaces;
+using System.Threading.Tasks;
+using WebAppTask2.Models;
 
-public interface IVisitRepository
+namespace WebAppTask2.Interfaces
 {
-    
+    public interface IVisitRepository
+    {
+        Task<IEnumerable<Visit>> GetAllForAnimal(int animalId);
+        void Add(Visit visit);
+        void Update(Visit visit);
+        void Delete(int id);
+        Task<Visit> GetById(int id);
+    }
 }
