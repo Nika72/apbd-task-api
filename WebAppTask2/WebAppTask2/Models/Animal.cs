d93f41691de50b92d0e04f63a81622d5656a9d67
s25686 <s25686@pjwstk.edu.pl>
2024-04-10 22:35:28 +0200

    Add Animal and Visit models with navigation and foreign key properties

--- WebAppTask2/WebAppTask2/Models/Animal.cs
+++ WebAppTask2/WebAppTask2/Models/Animal.cs
@@ -1,6 +1,15 @@
+using WebAppTask2.Models;
+
 namespace WebAppTask2.Models;
 
+// Animal.cs
 public class Animal
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; } 
+    public double Weight { get; set; }
+    public string FurColor { get; set; }
+    public List<Visit> Visits { get; set; } = new List<Visit>();
     
 }
--- WebAppTask2/WebAppTask2/Models/Visit.cs
+++ WebAppTask2/WebAppTask2/Models/Visit.cs
@@ -1,6 +1,18 @@
-namespace WebAppTask2.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Visit
+namespace WebAppTask2.Models
 {
-    
+    public class Visit
+    {
+        public int Id { get; set; }
+        public DateTime DateOfVisit { get; set; }
+        public double Price { get; set; }
+
+        // Foreign key property linking visits to animals
+        [ForeignKey("Animal")]
+        public int AnimalId { get; set; }
+
+        // Navigation property for accessing the related Animal entity
+        public Animal Animal { get; set; } = null!;
+    }
 }
